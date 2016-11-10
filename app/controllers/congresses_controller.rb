0c797042371c4f98af2a17eb1687bc3e0f438e9b
Jacques <jacques@sd-80088.dedibox.fr>
2016-11-10 09:20:10 +0100

    remove authentication and enable rack-cors

--- app/controllers/congresses_controller.rb
+++ app/controllers/congresses_controller.rb
@@ -1,8 +1,6 @@
 class CongressesController < ApplicationController
-  include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :set_congress, only: [:show, :update, :destroy]
-  before_action :authenticate
 
   # GET /congresses
   def index
@@ -58,25 +56,26 @@ class CongressesController < ApplicationController
 
   protected
 
-  # Authenticate the user with token based authentication
-  def authenticate
-    authenticate_token || render_unauthorized
-  end
+#  Authenticate the user with token based authentication
+  # def authenticate
+  #   authenticate_token || render_unauthorized
+  # end
 
-  def authenticate_token
-    authenticate_with_http_token do |token, options|
-      @current_user = User.find_by(api_key: token)
-    end
-  end
+  # def authenticate_token
+  #   authenticate_with_http_token do |token, options|
+  #     @current_user = User.find_by(api_key: token)
+  #   end
+  # end
 
-  def render_unauthorized(realm = "Application")
-    self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
-    render json: 'Bad credentials', status: :unauthorized
-  end
-
+  # def render_unauthorized(realm = "Application")
+  #   self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
+  #   render json: 'Bad credentials', status: :unauthorized
+  # end
+ 
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_congress
+    p params[:id]
     @congress = Congress.find(params[:id])
   end
 
